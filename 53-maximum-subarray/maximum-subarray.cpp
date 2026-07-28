eb522bbf6eb1b49073471f70caadcff216b3acd1
Ramakotesh Ramulapenta <ramulapentaramakotesh@gmail.com>
2026-07-28 19:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (14.86%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,17 @@
+//brute -> check the sum of all possible sub arrays
+//kadane's algorithm -> don't take negatives forward (no use)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        int maxSum = INT_MIN;
+
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+            maxSum = max(maxSum,sum);
+            if(sum<0) sum=0; 
+        }
+        return maxSum;
+    }
+};
